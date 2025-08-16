9a8ed49c511103694ceca4829f6c306b45cc8f65
jump6746 <jump6746@naver.com>
2025-08-16 20:30:02 +0900

    init: 환경 설정 및 디자인 시스템 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': '/src', // 프로젝트 루트 기준 src 폴더를 가리킴
+    },
+  },
 });
