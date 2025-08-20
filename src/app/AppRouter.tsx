cc41b6dcd813a87f110a2ca607c85c81d36ffbd3
서주원 <kciimnida@gmail.com>
2025-08-21 01:07:46 +0900

    merge

--- src/app/AppRouter.tsx
+++ src/app/AppRouter.tsx
@@ -1,11 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { AuctionLayout, MainPageLayout } from './layouts';
 import { AuctionRoomPage } from '@/pages/auction';
-<<<<<<< HEAD
 import { MainPage } from '@/pages/main';
-=======
 import { AutoBidPlace, BidPlace, DirectBuyPlace } from '@/widgets/auction/ui';
->>>>>>> 9410425dd05e0b3065123de341b26f63cf629208
 
 const AppRouter = createBrowserRouter([
   {
