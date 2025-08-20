25d17b9f38fa2227e638f3a2ff05f3d093b2af38
jump6746 <jump6746@naver.com>
2025-08-20 23:06:05 +0900

    fix: 빌드 에러 수정

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -5,9 +5,7 @@ export { default as TimerButton } from './TimerButton/TimerButton';
 export { default as customToast } from './CustomToast/customToast';
 export { default as TopNavigation } from './TopNavigation/TopNavigation';
 export { default as AuctionNotice } from './AuctionNotice/AuctionNotice';
-<<<<<<< HEAD
 export { default as Category } from './Category/Category';
-=======
 export { default as TextField } from './TextField/TextField';
 export {
   type CarouselApi,
@@ -17,4 +15,3 @@ export {
   CarouselPrevious,
   CarouselNext,
 } from './Carousel/carousel';
->>>>>>> 9410425dd05e0b3065123de341b26f63cf629208
